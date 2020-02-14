1399a1048de6268775399ab378e005b8a89a8c1b
Genada Zoto <genadazoto@hotmail.com>
2020-02-14 16:21:52 +0100

    final version

---
+++ GoGreen/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoGreen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string surname)
+        {
+            ViewBag.Name = name;
+            ViewBag.Surname = surname;
+            return View("Login", surname as object);
+        }
+
+        public ActionResult Login(string surname)
+        {
+            ViewBag.Surname = surname;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string surname, string email)
+        {
+            ViewBag.Surname= surname;
+            return View("Trip");
+        }
+
+        public ActionResult Trip()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Trip(string from, string to)
+        {
+            ViewBag.From = from;
+            ViewBag.To = to;
+            return View("TripResult");
+        }
+
+        public ActionResult TripResult()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TripResult(string trip)
+        {
+            ViewBag.Trip = trip;
+            return View("ChosenTrip");
+        }
+
+        public ActionResult Chosentrip()
+        {
+
+            return View();
+        }
+
+        public ActionResult Badges()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ GoGreen/Scripts/JsAnimation.js
@@ -0,0 +1,28 @@
+﻿
+
+ScrollReveal().reveal('.form-group', {
+    origin: 'left',
+    distance: '300px',
+    easing: 'ease-out',
+    duration: 1000,
+});
+ScrollReveal().reveal('.btn', {
+    origin: 'bottom',
+    distance: '300px',
+    easing: 'ease-out',
+    duration: 1000,
+});
+ScrollReveal().reveal('.form-group li', {
+    origin: 'top',
+    distance: '300px',
+    easing: 'ease-out',
+    duration: 1000,
+});
+
+
+ScrollReveal().reveal('#HeartImg', {
+    origin: 'bottom',
+    distance: '300px',
+    easing: 'ease-out',
+    duration: 1000,
+});
